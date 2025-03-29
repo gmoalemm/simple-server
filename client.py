bffae8538f5ca0ae5ac586faecffbfc022327001
Guy Moalem <guy.moa@gmail.com>
2025-03-29 16:22:19 +0300

    added greeting

--- client.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-BASE_URL = "http://127.0.0.1:5000"
-
-# Test home endpoint
-response = requests.get(BASE_URL)
-print(response.json())
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,12 @@ items = {}
 @app.route("/")
 def home():
     return jsonify({"message": "Welcome to the Flask API!"})
+
+@app.route("/greet")
+def greet():
+    name = request.args.get("name", "Guest")
+
+    return jsonify({"message": f"Hello, {name}!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
