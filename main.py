f8acbfacf823f75214034a78e56aa472220dd4d3
Guy Moalem <guy.moa@gmail.com>
2025-03-29 17:15:21 +0300

    ---

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-import os
-import sys
-print("Current working directory:", os.getcwd())
-print("Python path:", sys.path)
-
 from Server import Server
-from MenuGenerator import MenuGenerator
-import torch
+# from MenuGenerator import MenuGenerator
+# import torch
 
 MODEL_PATH = "model_weights.pth"
 
 if __name__ == "__main__":
-    # Load the model
-    print("Loading model...")
-    model = MenuGenerator()
-    model.load_state_dict(torch.load(MODEL_PATH))
-    model.eval()
+    # # Load the model
+    # print("Loading model...")
+    # model = MenuGenerator()
+    # model.load_state_dict(torch.load(MODEL_PATH))
+    # model.eval()
 
     # Create the server instance with the model
-    server = Server(model)
+    # server = Server(model)
 
-    # Run the server
-    print("Starting server...")
-    server.run()
+    # # Run the server
+    # print("Starting server...")
+    # server.run()
+
+    print("Server is not implemented yet.")
